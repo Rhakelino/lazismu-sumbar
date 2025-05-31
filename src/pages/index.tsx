5c2cf229728da68080da4d22485fb55a68e1919a
rakel lino <instanclay@gmail.com>
2025-05-31 18:55:19 +0700

    fix again and again and again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -132,7 +132,7 @@ const Home: React.FC = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                   </svg>
                 </div>
-                <p className="text-gray-600 italic mb-4">"{testimonial.message}"</p>
+                <p className="text-gray-600 italic mb-4">{`"${testimonial.message}"`}</p>
                 <h4 className="font-bold text-gray-800">{testimonial.name}</h4>
               </div>
             ))}
