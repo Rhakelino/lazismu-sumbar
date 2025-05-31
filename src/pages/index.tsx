040f9d238876c8dc825d6affae9aee3b93185f79
rakel lino <instanclay@gmail.com>
2025-05-31 20:27:05 +0700

    fix error kutip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -184,7 +184,7 @@ const Home: React.FC = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                   </svg>
                 </div>
-                <p className="text-gray-600 italic mb-4">"{testimonial.message}"</p>
+                <p className="text-gray-600 italic mb-4">&quot;{testimonial.message}&quot;</p>
                 <h4 className="font-bold text-gray-800">{testimonial.name}</h4>
               </motion.div>
             ))}
