545dbd821104b7a6edea0fe67e92dae79e0172c0
rakel lino <instanclay@gmail.com>
2025-06-01 14:37:05 +0700

    uninstall aos

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -1,11 +1,5 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
-import React, { useEffect, useState } from 'react';
-import AOS from 'aos'; // Import AOS
-import 'aos/dist/aos.css'; // Import AOS styles
-
-// Loading Spinner Component
-
 
 type AppShellProps = {
   children: React.ReactNode;
@@ -13,25 +7,7 @@ type AppShellProps = {
 
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
-  const [loading, setLoading] = useState<boolean>(true);
 
-  // Simulate loading time for demo purposes
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 300); // Simulate a loading time of 2 seconds
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  // Initialize AOS
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      duration: 600,
-      easing: 'ease-out-cubic',
-    });
-  }, []);
   return (
     <main className='h-full'>
       <Navbar />
