ab15b143386b92feffc8acc98ab7bd818577ca40
rakel lino <instanclay@gmail.com>
2025-06-05 17:45:23 +0700

    fix error

--- next.config.js
+++ next.config.js
@@ -8,4 +8,8 @@ module.exports = {
   images: {
     domains: ['picsum.photos'], // Tambahkan hostname yang ingin Anda izinkan
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
