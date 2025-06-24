a327fd2e40178d4205ac7468181272117d3cdad6
Rhakelino <instanclay@gmail.com>
2025-06-24 14:55:58 +0700

    add SEO

--- next.config.js
+++ next.config.js
@@ -2,14 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'picsum.photos',
-      'emchfippjpavibdpoppb.supabase.co'
-    ],
+    domains: ["picsum.photos", "emchfippjpavibdpoppb.supabase.co"],
   },
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/google5ac9ab3f46a5fc30.html",
+        destination: "/google5ac9ab3f46a5fc30.html",
+      },
+    ];
   },
 };
 
